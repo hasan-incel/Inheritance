3c922911f843dab953433a5c4d5b468ce7890d2e
Hasan İncel <h_incel@hotmail.com>
2024-08-10 17:43:31 +0300

    Add project files.

---
+++ Inheritance/BaseKisi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class BaseKisi
+    {
+        // Propertyler
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        // Konsol ekranına ad ve soyadı yazdıran metot
+        public void Bilgiler()
+        {
+            Console.WriteLine("\nAdı: " + Ad);
+            Console.WriteLine("Soyadı: " + Soyad);
+        }
+    }
+    // Ogrenci sınıfı BaseKisi'den miras alır
+    public class Ogrenci : BaseKisi
+    {
+        public int _ogrenciNo { get; set; }
+        public void OgrenciNo()
+        {
+            Bilgiler(); // BaseKisi'den gelen metodu çağırır
+            Console.WriteLine("Öğrenci No: " + _ogrenciNo);
+        }
+    }
+    // Ogretmen sınıfı BaseKisi'den miras alır
+    public class Ogretmen : BaseKisi
+    {
+        public int _maas { get; set; }
+        public void Maas()
+        {
+            Bilgiler(); // BaseKisi'den gelen metodu çağırır
+            Console.WriteLine("Maaş: " + _maas +"TL");
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Inheritance;
+
+// Öğretmen nesnesi oluşturuluyor
+Ogrenci ogrenci = new Ogrenci
+{
+    Ad = "Ali",
+    Soyad = "Yılmaz",
+    _ogrenciNo = 4567
+};
+
+// Öğretmen nesnesi oluşturuluyor
+Ogretmen ogretmen = new Ogretmen
+{
+    Ad = "Ayşe",
+    Soyad = "Kara",
+    _maas = 50000
+};
+
+ogrenci.OgrenciNo();
+ogretmen.Maas();
